f3a0016ea2dcf85d1c6ba8efe8c8ce23c2f4ad81
zunedismynamed <141910815+zunedismynamed@users.noreply.github.com>
2024-12-01 02:20:32 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,10 +6,10 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '8fe4e569-f5df-40bb-a9fa-c15d2e7434e3';
 
-const proxyIPs = ['128.199.218.226'];
+const proxyIPs = ['104.248.145.216'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
-let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
+let dohURL = 'https://cloudflare-dns.com/dns-query'; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
 // v2board api environment variables
 let nodeId = ''; // 1
